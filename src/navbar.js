f88c206d42a4ea629206711bb46f25305374125d
markopojer <marko.pojer@visokarazina.hr>
2022-07-28 15:24:15 +0200

    responsive menu done

---
+++ src/navbar.js
@@ -0,0 +1,12 @@
+// Grab HTML Elements
+const btn = document.getElementById("btn");
+const menu = document.querySelector(".mobile-menu");
+
+// // Add Event Listeners
+btn.addEventListener("click", () => {
+	menu.classList.toggle("hidden");
+});
+
+
+
+
